3a35d41de57c34f32551c1a4d23d558aa04f688a
Fábio Gonçalves de Camargo <fabiogdecamargo@gmail.com>
2020-04-11 19:41:28 -0300

    Acao com inicialização como paramentro (Consumer)

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -11,7 +11,7 @@ public class DepartmentService {
 		List<Department> list = new ArrayList<>();
 		list.add(new Department(1, "Livros"));
 		list.add(new Department(2, "Computadores"));
-		list.add(new Department(3, "Eletronicos"));
+		list.add(new Department(3, "Eletronicos"));		
 		return list;
 	}
 }
